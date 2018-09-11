9345b592c1fd4080fcfe02d5c86b8ab9231a86bf
Sean <xuelun.lu@gmail.com>
2018-09-11 10:35:23 +0800

    update

---
+++ resources.php
@@ -0,0 +1,56 @@
+<?php include('top.php'); ?>
+<h3>網頁設計</h3>
+<table class="table table-bordered">
+	<thead>
+		<tr>
+			<th>服務名稱</th><th>&nbsp;</th><th>服務名稱</th><th>&nbsp;</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><a href="https://www.elegantthemes.com/" target="_blank">Elegant</a></td>
+			<td>wordpress佈景</td>
+			<td><a href="https://fontawesome.com/" target="_blank">Font Awesome</a></td>
+			<td>icon</td>
+		</tr>
+		<tr>
+			<td><a href="http://www.webpage.com.tw/" target="_blank">webpage</a></td>
+			<td>網頁設計佈景</td>
+		</tr>
+		<tr>
+			<td><a href="https://themeforest.net/" target="_blank">envatomarket</a></td>
+			<td>網頁設計佈景</td>
+		</tr>
+		<tr>
+			<td><a href="https://www.templateplazza.com/" target="_blank">TemplatePlazza</a></td>
+			<td>joomla</td>
+		</tr>
+		<tr>
+			<td><a href="https://www.themeparrot.com/" target="_blank">themeparrot</a></td>
+			<td>Joomla Templates & WordPress Themes</td>
+		</tr>
+	</tbody>
+</table>
+<h3>python</h3>
+<table class="table table-bordered">
+	<thead>
+		<tr>
+			<th>服務名稱</th><th>&nbsp;</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><a href="https://radimrehurek.com/gensim/" target="_blank">gensim</a></td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td><a href="https://www.anaconda.com/" target="_blank">anaconda</a></td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td><a href="https://repo.continuum.io/archive/" target="_blank">Anaconda installer archive</a></td>
+			<td>&nbsp;</td>
+		</tr>
+	</tbody>
+</table>
+<?php include('bottom.php'); ?>
